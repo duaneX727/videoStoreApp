20c88f1289dc2b110ce56c18d7437c3261977ab1
duaneX727 <mdmitch7@gmail.com>
2020-10-13 06:17:48 -0400

    Initial app upload
    Initial app upload, just prior to route install

---
+++ src/components/common/pages/list-group.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ListGroup = ({items,selectedItem, onItemSelect,textProperty, valueProperty}) => {
+    return ( 
+    <ul className="list-group">
+      {items.map(item => (
+        <li 
+            key={item[valueProperty]}
+            className={item === selectedItem ?"list-group-item active":"list-group-item"}
+            style={{cursor: 'pointer'}} 
+            onClick={()=> onItemSelect(item)}>
+            {item[textProperty]}
+        </li>))
+      }
+    </ul>
+       );
+}
+ListGroup.defaultProps = {
+  textProperty: 'name',
+  valueProperty: '_id'
+}
+ListGroup.propTypes = {
+  items: PropTypes.array.isRequired,
+  onItemSelect: PropTypes.func.isRequired,
+  selectedItem: PropTypes.object, 
+}
+export default ListGroup;
